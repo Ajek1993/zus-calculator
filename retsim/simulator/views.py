34daea4f7ab7a0fdaba3fc641c50c0a40b69ed53
Dawid Kapusta <dawidkapusta67@gmail.com>
2025-10-04 17:26:57 +0200

    szkielet

---
+++ retsim/simulator/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'simulator/index.html')
+
+def start_simulation(request):
+    pass
